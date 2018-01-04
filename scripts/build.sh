bfcc735f22448265d10f86a918eead7ac4c3d20e
Issam SAID <isaid@nvidia.com>
2018-01-04 15:11:21 +0100

    add building scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,3 @@
+# -DCMAKE_LIBRARY_PATH=${CUDA_HOME}/lib64/stubs
 cmake  -Dezcu_debug=off -DNVRTC_LIBRARY=${CUDA_HOME}/lib64/stubs/libnvrtc.so -DCUDA_CUDA_LIBRARY=${CUDA_HOME}/lib64/stubs/libcuda.so -DCMAKE_C_COMPILER=icc  -DCMAKE_CXX_COMPILER=icpc -DCMAKE_Fortran_COMPILER=ifort -G "Unix Makefiles" ../
 
